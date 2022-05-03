2be50fe398ceac5cae442db614ed0a1e3f02c93b
Aashutosh <aashutoshkumar869@gmail.com>
2022-05-03 14:45:38 +0530

    some folder rearrangement

---
+++ NOTES/basic.js
@@ -0,0 +1,5 @@
+let a = 0.1;
+
+a = a*a;
+
+console.log(a);
---
+++ NOTES/javascript/getting and updating data to and from objects.js
@@ -0,0 +1,51 @@
+// Setup
+const recordCollection = {
+    2548: {
+        albumTitle: 'Slippery When Wet',
+        artist: 'Bon Jovi',
+        tracks: ['Let It Rock', 'You Give Love a Bad Name']
+    },
+    2468: {
+        albumTitle: '1999',
+        artist: 'Prince',
+        tracks: ['1999', 'Little Red Corvette']
+    },
+    1245: {
+        artist: 'Robert Palmer',
+        tracks: []
+    },
+    5439: {
+        albumTitle: 'ABBA Gold'
+    }
+};
+
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+  if (prop !== "tracks" || prop !== "artist" || prop !== "") {
+    // value = prop;
+  }
+
+  if (prop === "tracks" && value !== "" && records[id].tracks !== []) {
+    records[id].tracks = [];
+    records[id].tracks.push(value);
+  } else if (prop === "tracks" && value !== "") {
+    records[id].tracks.push(value);
+  }
+
+  if (value === "") {
+    delete records[id][prop];
+  }
+  console.log(`----------------------------------------------------------`);
+  console.log(recordCollection);
+  return recordCollection;
+}
+
+
+
+updateRecords(recordCollection, 5439, 'artist', 'ABBA')
+updateRecords(recordCollection, 5439, "tracks", "Take a Chance on Me");
+updateRecords(recordCollection, 2548, "artist", "");
+updateRecords(recordCollection, 1245, "tracks", "Addicted to Love")
+updateRecords(recordCollection, 2468, "tracks", "Free")
+updateRecords(recordCollection, 2548, "tracks", "")
+updateRecords(recordCollection, 1245, "albumTitle", "Riptide")
